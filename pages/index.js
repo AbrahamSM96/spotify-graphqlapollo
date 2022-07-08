5ce61c21400b5870ea1b7694ada02ccbbc34d3dd
Abraham Serrano <abrahamserrano96@hotmail.com>
2022-07-08 14:51:38 -0500

    ✨ add intro page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-import { Button, Text } from "@nextui-org/react";
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
-import { gql, useQuery } from "@apollo/client";
-import SearchBar from "../src/components/SearchBar";
-import { FindArtist } from "@hoc/FindArtist";
+import Link from 'next/link'
+import { useState, useEffect } from 'react'
+import { Grid, Text, Button, Spacer } from '@nextui-org/react'
+import Head from 'next/head'
+import styles from '../styles/Index.module.css'
+import confetti from 'canvas-confetti'
 
 // const query = gql`
 //   query {
@@ -20,8 +20,29 @@ import { FindArtist } from "@hoc/FindArtist";
 //   }
 // `;
 
-export default function Home() {
+export default function Home () {
   // const result = useQuery(query);
+  const [isMobile, setIsMobile] = useState(true)
+
+  const mobile = (agent) => {
+    const AGENT = agent.toLowerCase()
+    const REGEXP = /android|webos|iphone|ipod|ipad|blackberry/
+    return REGEXP.test(AGENT)
+  }
+  useEffect(() => {
+    const agent = navigator.userAgent
+    console.log(agent, 'agent')
+    setIsMobile(mobile(agent))
+  }, [isMobile])
+
+  const handleConfetti = () => {
+    confetti({
+      particleCount: 150,
+      spread: 180
+
+    })
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -31,9 +52,78 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <SearchBar />
-        <FindArtist />
+        <Grid.Container gap={2} justify="center">
+          <Grid xs="4" md="8">
+            <Text
+              h1
+              size={isMobile ? 80 : 130}
+              css={{
+                textAlign: 'center',
+                letterSpacing: '-.06rem',
+                margin: '0 0 16px'
+              }}
+            >
+              <span
+                className={`${styles.span} ${styles._spotify}`}
+                defaultValue="Spotify"
+              >
+                Spotify
+              </span>
+              <span className={`${styles.span} ${styles._artist}`}>Artist</span>
+              <span className={`${styles.span} ${styles._searcher}`}>
+                Searcher
+              </span>
+              <Spacer/>
+              <Link href='/home'>
+
+              <Button
+                auto
+                rounded
+                ripple={false}
+                size="xl"
+                onClick={handleConfetti}
+                css={{
+                  background: '#2fe6af',
+                  fontWeight: '$semibold',
+                  boxShadow: '$md',
+                  position: 'relative',
+                  overflow: 'visible',
+                  color: '#2fe6af',
+                  px: '$18',
+                  margin: '0 auto',
+                  '&:after': {
+                    content: '""',
+                    position: 'absolute',
+                    width: '100%',
+                    height: '100%',
+                    background: '$white',
+                    opacity: 1,
+                    borderRadius: '$pill',
+                    transition: 'all 0.4s ease'
+                  },
+                  '&:hover': {
+                    color: '$white',
+                    transform: 'translateY(-5px)',
+                    '&:after': {
+                      transform: 'scaleX(1.5) scaleY(1.6)',
+                      opacity: 0
+                    }
+                  },
+                  '&:active': {
+                    transform: 'translateY(-2px)'
+                  }
+                }}
+                >
+                  Click me
+                </Button>
+                </Link>
+
+            </Text>
+
+          </Grid>
+
+        </Grid.Container>
       </main>
     </div>
-  );
+  )
 }
