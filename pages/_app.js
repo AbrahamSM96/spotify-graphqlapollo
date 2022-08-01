56e4ac5c9da5d6ef7aadd787a383c8cd8cc4be2d
Abraham Serrano <abrahamserrano96@hotmail.com>
2022-08-01 13:26:48 -0500

    feat: lint fix and run build

--- pages/_app.js
+++ pages/_app.js
@@ -1,41 +1,41 @@
-import "../styles/globals.css";
-import { createTheme } from "@nextui-org/react";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
+import '../styles/globals.css'
+import { createTheme } from '@nextui-org/react'
+import { ThemeProvider as NextThemesProvider } from 'next-themes'
 import {
   ApolloClient,
   ApolloProvider,
-  gql,
   HttpLink,
-  InMemoryCache,
-} from "@apollo/client";
-import { Provider } from "react-redux";
-import { store } from "../src/app/store";
+  InMemoryCache
+} from '@apollo/client'
+import { Provider } from 'react-redux'
+import { store } from '../src/app/store'
 const lightTheme = createTheme({
-  type: "light",
+  type: 'light',
   theme: {
     colors: {
-      background: "#101727",
-    },
-  },
-});
+      background: '#101727'
+    }
+  }
+})
 const darkTheme = createTheme({
-  type: "dark",
+  type: 'dark',
   theme: {
     colors: {
-      background: "#101727",
-    },
-  },
-});
+      background: '#101727'
+    }
+  }
+})
 
 const client = new ApolloClient({
   connectToDevTools: true,
   cache: new InMemoryCache(),
   link: new HttpLink({
-    uri: process.env.NEXT_PUBLIC_SPOTIFY_API,
-  }),
-});
+    uri: process.env.NEXT_PUBLIC_SPOTIFY_API
+  })
+})
 
-function MyApp({ Component, pageProps }) {
+// eslint-disable-next-line react/prop-types
+function MyApp ({ Component, pageProps }) {
   return (
     <ApolloProvider client={client}>
       <Provider store={store}>
@@ -48,7 +48,7 @@ function MyApp({ Component, pageProps }) {
         </NextThemesProvider>
       </Provider>
     </ApolloProvider>
-  );
+  )
 }
 
-export default MyApp;
+export default MyApp
